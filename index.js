451b570fc150a55df99257ca29af08537536fdac
kelvinachiando <kelvinostin22gmail.com>
2022-06-06 21:49:54 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Kelvin Achiando";
+
+const welcomeMessage = "Welcome to Flatbook, ${Kelvin Achiando}!";
+
+const excitedWelcomeMessage = welcomeMessage. toUpperCase();
+
+const shortGreeting =`Welcome, ${currentUser.slice(0, 1)}!`;
